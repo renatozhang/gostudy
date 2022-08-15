76958198cb40c64bc319aca678ed1acb62398f7e
Zz <865548937@qq.com>
2022-08-15 15:09:20 +0800

    lesson11

---
+++ lesson11/calc/calc.go
@@ -0,0 +1,20 @@
+package calc
+
+import "fmt"
+
+var (
+	Sum int
+	// sub int
+)
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func init() {
+	fmt.Println("init in calc")
+}
