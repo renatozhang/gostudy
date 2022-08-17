6798117e14d31345593991b12da7433e702333f2
Zz <865548937@qq.com>
2022-08-17 22:08:54 +0800

    modified:   go.mod 	modified:   go.sum

---
+++ lesson14/cli_example/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "greet"
+	app.Action = func(c *cli.Context) error {
+		var cmd string
+		if c.NArg() > 0 {
+			cmd = c.Args()[0]
+		}
+		fmt.Println("hello firend! cmd:", cmd)
+		return nil
+	}
+	app.Run(os.Args)
+}
